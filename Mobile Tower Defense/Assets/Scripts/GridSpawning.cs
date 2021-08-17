3722dcc9d2334bcafb2168526227b17839915f2f
ToddPoly <meast2@student.op.ac.nz>
2021-08-17 19:04:03 +1200

    Basic grid layout of tiles with colliders done

---
+++ Mobile Tower Defense/Assets/Scripts/GridSpawning.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawning : MonoBehaviour
+{
+    public GameObject prefab;
+    public float gridX = 5f;
+    public float gridY = 5f;
+    public float spacingX = 2f;
+    public float spacingY = 2f;
+
+    void Start()
+    {
+        for (int y = 0; y < gridY; y++)
+        {
+            for (int x = 0; x < gridX; x++)
+            {
+                Vector3 pos = new Vector3(this.transform.localPosition.x + (x * spacingX) + 1, 0, this.transform.localPosition.z + (y * spacingY) + 0.5f);//super scuffed way of added offset
+                GameObject newTile = Instantiate(prefab, pos, Quaternion.identity, this.transform);
+            }
+        }
+    }
+}
