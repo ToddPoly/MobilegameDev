03da446bca108a2551ef2f6d4603e0d5f7008b5b
ToddPoly <meast2@student.op.ac.nz>
2021-08-19 14:26:16 +1200

    Setup basic drag ability to drag a tower off of the UI onto the world scene to spawn towers

--- Mobile Tower Defense/Assets/Scripts/InputManager.cs
+++ Mobile Tower Defense/Assets/Scripts/InputManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
     public GameObject ground;
     public bool spawnedObject = false;
+    public GameObject tower;
     private GameObject towerDragInstance;
 
     private void Update()
@@ -16,37 +18,40 @@ public class InputManager : MonoBehaviour
             Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 12));//z (12) is distance from camera
             Ray ray = Camera.main.ScreenPointToRay(touch.position);           
             int id = touch.fingerId;
-            //Debug.DrawRay(Camera.main.transform.position, touchedPos, Color.green);
 
             Debug.Log(touch.phase);
-            if (touch.phase == TouchPhase.Began)//Read the raycast hit collider UI tower panel and instatiate the tower prefab in the player touch point
+            if (touch.phase == TouchPhase.Began)
             {
-                RaycastHit hitInfo;
-                if (Physics.Raycast(ray, out hitInfo))
+                if (EventSystem.current.IsPointerOverGameObject(id))//Checking if touch is on UI then will set tower only if a UI event has passed a gameobject to the class GetTower below
                 {
-                    var hit = hitInfo.collider.name;
-                    if (hit != null)
+                    Debug.Log("On UI");
+                    if (!spawnedObject && tower)
                     {
-                        Debug.Log(hit);
+                        //check touched ui tower element
+                        //Set tower towerDragInstance to the gameObject of the UI tower element you have touched
+                        towerDragInstance = Instantiate(tower, ground.transform);
+
+                        towerDragInstance.transform.position = touchedPos;
+
+                        spawnedObject = true;
                     }
                 }
-                if (!spawnedObject)
+                else// Only run when not on UI, might at ability to move towers or upgrade/sell them later using a raycast on their colliders
                 {
-                    //check touched ui tower element
-                    //Set tower towerDragInstance to the gameObject of the UI tower element you have touched
-                    //towerDragInstance = Instantiate(, ground.transform);
-
-                    towerDragInstance.transform.position = touchedPos;
-
-                    spawnedObject = true;
+                    Debug.Log("Not UI");
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(ray, out hitInfo))
+                    {
+                        var hit = hitInfo.collider.name;
+                        if (hit != null)
+                        {
+                            Debug.Log(hit);
+                        }
+                    }
                 }
             }
-            else
-            {
-                Debug.Log("Not UI");
-            }
 
-            if (touch.phase == TouchPhase.Moved)
+            if (touch.phase == TouchPhase.Moved)//Read the raycast gridtile and hightlight it in game showing where the tower will go
             {
                 if (spawnedObject)
                 {
@@ -54,13 +59,19 @@ public class InputManager : MonoBehaviour
                 }
             }
 
-            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)//Read the raycast grid tile location from the moved or ended then spawn the held tower at that location then delete it in this class
             {
                 Destroy(towerDragInstance);
+                tower = null;
                 spawnedObject = false;
             }
 
             Debug.Log(spawnedObject);
         }
+    }
+
+    public void GetTower(GameObject gameObject)
+    {
+        tower = gameObject;
     }
 }
