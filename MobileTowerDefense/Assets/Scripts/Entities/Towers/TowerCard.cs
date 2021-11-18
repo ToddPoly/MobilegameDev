f74306634c39f7e6c2cd5b4365ccbb3ebfdeab06
Todd Meason (1000027339) <MEAST2@student.op.ac.nz>
2021-11-19 10:06:08 +1300

    Add name to tower cards and got build working

--- MobileTowerDefense/Assets/Scripts/Entities/Towers/TowerCard.cs
+++ MobileTowerDefense/Assets/Scripts/Entities/Towers/TowerCard.cs
@@ -11,12 +11,13 @@ public class TowerCard : MonoBehaviour //change whole class to an input manager 
     public Image tower_Icon;
 
     public int cost;
+    public string name;
     public TextMeshProUGUI costDisplay;
 
     private void Start()
     {
         //cost = get the tower cost once the tower is put in proplery not on button event trigger
         costDisplay = GetComponentInChildren<TextMeshProUGUI>();
-        costDisplay.text = cost.ToString();
+        costDisplay.text = name + " " + cost.ToString();
     }
 }
